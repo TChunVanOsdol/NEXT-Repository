a4dc75619d149c3929cdb45d7a7d2e70c411e5c5
TChunVanOsdol <32657616+TChunVanOsdol@users.noreply.github.com>
2018-06-24 15:05:55 -0700

    Update HelloNext.py

--- HelloNext.py
+++ HelloNext.py
@@ -1,3 +1,3 @@
 # Fill out the print function and submit a pull request
-# with a directory using your name (ex. ToddCVO/HelloNext.py)
+# with a directory using your name (ex. /ToddCVO/HelloNext.py)
 print("")
